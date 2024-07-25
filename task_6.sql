cabd1292ebcdd63340381159909024674ef2ff3d
Masri <masrialemu404@gmail.com>
2024-07-25 13:59:48 +0300

    v0.7

--- task_6.sql
+++ task_6.sql
@@ -1,3 +1,6 @@
-INSERT INTO Customers (customer_id, customer_name, email, address) 
+USE alx_book_store;
+INSERT INTO customers (customer_id, customer_name, email, address) 
 VALUES
 (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness__Ave.'),
+(3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happines__Ave.'),
+(4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness__Ave.');
