565137d91a8b1fdbc97f4422c8637e491b378cef
Masri <masrialemu404@gmail.com>
2024-07-25 13:57:53 +0300

    v0.6.1

--- task_6.sql
+++ task_6.sql
@@ -1,3 +1,3 @@
 INSERT INTO Customers (customer_id, customer_name, email, address) 
 VALUES
-(2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
+(2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness__Ave.'),
