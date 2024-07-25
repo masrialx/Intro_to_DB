34f86a8e73b419201de2e932d246a382a4714dea
Masri <masrialemu404@gmail.com>
2024-07-25 12:58:48 +0300

    v0.2

--- task_4.sql
+++ task_4.sql
@@ -1,3 +1,15 @@
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'books';
+-- Use the specified database (you will provide the database name when running the script)
+USE alx_book_store;
+
+-- Query the information_schema to get the full description of the Books table
+SELECT 
+    COLUMN_NAME AS 'Column Name',
+    COLUMN_TYPE AS 'Column Type',
+    IS_NULLABLE AS 'Is Nullable',
+    COLUMN_KEY AS 'Key',
+    EXTRA AS 'Extra Info'
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store' AND
+    TABLE_NAME = 'Books';
