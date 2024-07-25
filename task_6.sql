d9c3f9b12e2a50ec2582fd70a1fd5d79c2da78e8
Masri <masrialemu404@gmail.com>
2024-07-25 13:01:47 +0300

    v0.2

--- task_6.sql
+++ task_6.sql
@@ -1,4 +1,8 @@
-INSERT INTO customers (customer_id, customer_name, email, address)
+-- Use the specified database (you will provide the database name when running the script)
+USE alx_book_store;
+
+-- Insert multiple rows into the Customers table
+INSERT INTO Customers (customer_id, customer_name, email, address)
 VALUES
     (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
     (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
