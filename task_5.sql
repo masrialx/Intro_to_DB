17c793c203e4519cc2e2786b410cf87e295411b5
Masri <masrialemu404@gmail.com>
2024-07-25 13:12:16 +0300

    v0.4

--- task_5.sql
+++ task_5.sql
@@ -1,3 +1,2 @@
--- Insert a single row into the Customers table
-INSERT INTO Customers (customer_id, customer_name, email, address) VALUES
-(1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+INSERT INTO customers (customer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
